8df7ff21031a67f0e2057d287f229e7907b59e3f
Ian Martinez <imartinez0753@gmail.com>
2020-05-24 18:07:26 -0700

    I have no idea what I am doing here

--- week-3-javascript/assets/js/script.js
+++ week-3-javascript/assets/js/script.js
@@ -1,25 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-	// you can create a function named generatePassword that creates the password
-	var password = generatePassword();
-	var passwordText = document.querySelector("#password");
-
-	passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-// -------------------------------------------------------------------------
-function loop(arr) {
-	for (i = 0; i < characterLength; i++);
-	result = arr[Math.floor(Math.random() * characterLength) + 1];
-	console.log(result);
-}
-
 var lower = [
 	"a",
 	"b",
@@ -105,59 +83,79 @@ var special = [
 ];
 var num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-var characterLength = prompt(
-	"how many characters would you like in your password?\nYou must choose between 8 and 128 characters"
-);
-if (characterLength >= 8 && characterLength <= 128) {
-	alert("your password will be " + characterLength + " characters long.");
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+function generatePassword() {
+	var characterLength = prompt(
+		"how many characters would you like in your password?\nYou must choose between 8 and 128 characters"
+	);
+	characterLength = parseInt(characterLength);
+	if (characterLength >= 8 && characterLength <= 128) {
+		alert("your password will be " + characterLength + " characters long.");
+	} else {
+		return alert("Invalid entry");
+	}
 
 	var userLower = confirm("Would you like to use lowercase characters?");
 	var userUpper = confirm("Would you like to use uppercase characters?");
 	var userNum = confirm("Would you like to use numbers?");
 	var userSpecial = confirm("Would you like to use special characters?");
-	characterLength = parseInt(characterLength);
+	var collection = [];
+
+	if (userLower === true) {
+		collection = collection.concat(lower);
+		console.log(collection);
+		//push to new array
+	}
+	//if only one is used
+	if (
+		//push to new array
+		userUpper === true
+	) {
+		collection = collection.concat(upper);
+		//randomize and output
+	}
+	//if all but num is chosen
+	if (userNum === true) {
+		collection = collection.concat(num);
+		console.log(collection);
+		// push to new array
+	}
+	//if all but lower is used
+	if (userSpecial === true) {
+		collection.concat(special);
+		// push to new array combine randomiz and output
+	}
+	console.log(collection);
+}
+// Write password to the #password input
+function writePassword() {
+	// you can create a function named generatePassword that creates the password
+	var password = generatePassword();
+	var passwordText = document.querySelector("#password");
+
+	passwordText.value = password;
 }
 
-//if all are aplied
-if (
-	userLower === true &&
-	userUpper === true &&
-	userSpecial === true &&
-	userNum === true
-) {
-	//combine randomize and output
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// -------------------------------------------------------------------------
+
+function loop(arr) {
+	for (var i = 0; i < characterLength; i++) {
+		result = arr[Math.floor(Math.random() * characterLength)];
+		console.log(result);
+	}
 }
-//if only one is used
-else if (
-	userLower === true ||
-	userUpper === true ||
-	userNum === true ||
-	userSpecial === true
-) {
-	//randomize and output
-}
-//if all but num is chosen
-else if (
-	userNum === false &&
-	userLower === true &&
-	userUpper === true &&
-	userSpecial === true
-) {
-	//combine randomize and output
-}
-//if all but lower is used
-else if (
-	userLower === false &&
-	userUpper === true &&
-	userSpecial === true &&
-	userNum === true
-) {
-	//combine randomiz and output
-}
+loop(collection);
+
+console.log(collection);
+console.log("-------------------");
 
 // loop(num);
 // console.log("------------------");
-loop(lower);
+// loop(lower);
 // console.log("------------------");
 // loop(upper);
 // console.log("------------------");
